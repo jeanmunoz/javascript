ae17b27aafbffb0d834d20be9f64280f220cd7fb
Jean Munoz <82293694+jeanmunoz@users.noreply.github.com>
2021-04-23 12:56:03 -0400

    Create Fundamentos Basicos I (Los 13 Algoritmos basicos).js

---
+++ Fundamentos Basicos I (Los 13 Algoritmos basicos).js
@@ -0,0 +1,9 @@
+// 1.- Obtén del 1 al 255: Escribe una función que devuelve un array con todos los números del 1 al 255.
+function Array1Hasta255(){
+var arr=[];
+for(i=1;i<=255;i++){
+arr.push(i);
+}
+console.log(arr);
+}
+Array1Hasta255()
